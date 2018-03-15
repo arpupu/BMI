f5e71c07e50cb06a3fb4d8c4e458b09345f406c5
m305-63\w401 <a106082006@mail.shu.edu.tw>
2018-03-15 18:09:47 +0800

    5454654

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        
+            Console.WriteLine("請輸入身高");
+            Console.WriteLine("請輸入體重");
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            double a = double.Parse(input2) / (double.Parse(input1) / 100 * double.Parse(input1) / 100);
+
+            
+            
+
+
+            Console.WriteLine("BMI="+Math .Round ( a));
+            Console.Read();
+
+            
+        }
+    }
+}
